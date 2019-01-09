ad0e4024d49aab726909ad6a648cd87516da1c77
Sophie King <sophiek89@hotmail.com>
2019-01-09 17:04:57 +0000

    Fix bugs for the gh-user-data & gh-repo-data empty divs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -36,6 +36,10 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+    
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -70,3 +74,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
