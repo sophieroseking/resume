4577d9ba4883ceec0dc3461fbc91d64b819a7404
Sophie King <sophiek89@hotmail.com>
2019-01-11 14:07:30 +0000

    Implement emailjs integration on the contact page

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -19,27 +19,26 @@ function repoInformationHTML(repos) {
     if (repos.length == 0) {
         return `<div class="clearfix repo-list">No repos!</div>`;
     }
+
     var listItemsHTML = repos.map(function(repo) {
         return `<li>
-            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-            </li>`;
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
     });
 
     return `<div class="clearfix repo-list">
-            <p>
-                <strong>Respo List:</strong>
-            </p>
-            <ul>
-            ${listItemsHTML.join("\n")}
-            </ul>
-    </div>`;
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
 }
 
 function fetchGitHubInformation(event) {
-    
     $("#gh-user-data").html("");
     $("#gh-repo-data").html("");
-    
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -66,8 +65,10 @@ function fetchGitHubInformation(event) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
-            }
-            else {
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+            } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
                     `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
